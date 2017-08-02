e15d3815ceb859e8e48bf7f1db5269dd12caa136
Raymart <raymart.sarmiento23@gmail.com>
2017-08-01 21:07:23 -0700

    Added Anti advertise

--- src/org/chatmanager/listeners/AntiAdvertise.java
+++ src/org/chatmanager/listeners/AntiAdvertise.java
@@ -6,8 +6,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.chatmanager.ChatManager;
 import org.chatmanager.api.ApiManager;
-
-import java.util.Arrays;
+import java.util.List;
 
 public class AntiAdvertise implements Listener {
     private ApiManager apiManager = ChatManager.getApi();
@@ -24,21 +23,15 @@ public class AntiAdvertise implements Listener {
 
         String numbers = "[0-9]";
         boolean advertise = false;
-        String[] webs = ChatManager.getInstance().getConfig().getStringList("websiteMatcher").toArray(new String[ChatManager.getInstance().getConfig().getStringList("websiteMatcher").size()]);
+        List<String> webs = ChatManager.getInstance().getConfig().getStringList("websiteMatcher");
         if(!e.getMessage().contains(" ")) {
             if(!e.getMessage().contains(".")) {
                 return;
             }
-            String[] ipOrElse = e.getMessage().split(".");
-            for(String web : Arrays.asList(webs)) {
-                if(ipOrElse[0].equalsIgnoreCase(web) || ipOrElse[0].matches(numbers)) {
-                    advertise = true;
-                }
-                if(ipOrElse[1].matches(numbers)) {
-                    advertise = true;
-                }
-                if(ipOrElse[2] != null) {
-                    if(ipOrElse[2].equalsIgnoreCase(web) || ipOrElse[2].matches(numbers)) {
+            String[] ipOrElse = e.getMessage().split("\\.");
+            for(String web : webs) {
+                for(int i = 0; i < ipOrElse.length; i++) {
+                    if(ipOrElse[i].equalsIgnoreCase(web) || ipOrElse[i].matches(numbers)) {
                         advertise = true;
                     }
                 }
@@ -49,16 +42,10 @@ public class AntiAdvertise implements Listener {
         if(e.getMessage().contains(" ")) {
             for(String message : e.getMessage().split(" ")) {
                 if(message.contains(".")) {
-                    String[] ipOrElse = message.split(".");
-                    for(String web : Arrays.asList(webs)) {
-                        if(ipOrElse[0].equalsIgnoreCase(web) || ipOrElse[0].matches(numbers)) {
-                            advertise = true;
-                        }
-                        if(ipOrElse[1].matches(numbers)) {
-                            advertise = true;
-                        }
-                        if(ipOrElse[2] != null) {
-                            if(ipOrElse[2].equalsIgnoreCase(web) || ipOrElse[2].matches(numbers)) {
+                    String[] ipOrElse = message.split("\\.");
+                    for(String web : webs) {
+                        for(int i = 0; i < ipOrElse.length; i++) {
+                            if(ipOrElse[i].equalsIgnoreCase(web) || ipOrElse[i].matches(numbers)) {
                                 advertise = true;
                             }
                         }
